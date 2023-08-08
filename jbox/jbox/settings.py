fb57c4fdce368b6a714b167127fec4e9cdfe12e7
Andrea <andrea.barnabo92@gmail.com>
2023-08-08 11:50:46 +0200

    passo indietro

--- jbox/jbox/settings.py
+++ jbox/jbox/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/home/9barna2/jukebox/jbox-fe/build/static"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     FRONTEND_DIR / "build/static",
 ]
